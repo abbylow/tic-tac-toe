e6fb4255b7d7f13c1bf7e8d294ef56d9ade8b39d
abbylow <yu950713@gmail.com>
2020-01-29 16:58:50 +0800

    Updates

---
+++ precache-manifest.865140cc182b06f73300af5f6b04213f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce0efe02c30e99ba424354bceca8dfc",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "26f36d2235193f590db9",
+    "url": "/tic-tac-toe/static/css/main.96a45a35.chunk.css"
+  },
+  {
+    "revision": "44cac84b209f70257e97",
+    "url": "/tic-tac-toe/static/js/2.bb21a5fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.bb21a5fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26f36d2235193f590db9",
+    "url": "/tic-tac-toe/static/js/main.b838fc87.chunk.js"
+  },
+  {
+    "revision": "14c00c4b9a734bdeb7a0",
+    "url": "/tic-tac-toe/static/js/runtime-main.1fec3c3c.js"
+  }
+]);
